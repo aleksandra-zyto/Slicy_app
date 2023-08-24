764349c80a4e6b3e73fd809c32dc6768b21f1b85
Ola Zyto <ola.zyto606@gmail.com>
2023-08-24 10:06:33 +0100

    Initial Commit

---
+++ Slicy/SlicyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlicyApp.swift
+//  Slicy
+//
+//  Created by Ola Żyto on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SlicyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
